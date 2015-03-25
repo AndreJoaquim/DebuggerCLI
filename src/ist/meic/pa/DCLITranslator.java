1e7b0a2912b6beea00a1d8a126f0a209155d1e42
Diogo Rato <diogo.rato.11@gmail.com>
2015-03-25 12:34:06 +0000

    Return almost done!

--- src/ist/meic/pa/DCLITranslator.java
+++ src/ist/meic/pa/DCLITranslator.java
@@ -1,13 +1,21 @@
 package ist.meic.pa;
 
+import java.util.HashMap;
+
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtConstructor;
 import javassist.CtMethod;
+import javassist.CtNewMethod;
 import javassist.NotFoundException;
 import javassist.Translator;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
 
 public class DCLITranslator implements Translator {
+	
+	private HashMap<String, String> debuggedMethods = new HashMap<String, String>();
 
 	@Override
 	public void onLoad(ClassPool pool, String className) throws NotFoundException,
@@ -25,12 +33,72 @@ public class DCLITranslator implements Translator {
 
 	@Override
 	public void start(ClassPool pool) throws NotFoundException,
-	CannotCompileException {		
+											 CannotCompileException {		
 	}
 
-	void insertDebugCode(CtClass ctClass) throws NotFoundException, CannotCompileException, 	ClassNotFoundException{
+	void insertDebugCode(CtClass ctClass) throws NotFoundException,
+												 CannotCompileException,
+												 ClassNotFoundException {
+		
+		// Edit all the constructors
+		for(CtConstructor ctConstructor: ctClass.getConstructors()){
+			
+		}
+		
+		// Edit all the remaining methods
 		for(CtMethod ctMethod: ctClass.getDeclaredMethods()) {
-			//TODO: Edit Methods
+			
+			//Para cada metodo editar todas as methods call
+			ctMethod.instrument(new ExprEditor(){
+				public void edit(MethodCall m) throws CannotCompileException {
+					
+					// Old method name
+					String methodName = m.getMethodName();
+									
+					// New method name
+					String newMethodName = methodName + "$debug";
+					
+					
+					// Replace the method call
+					// $1: First parameter
+					// $_: Resulting value of the method call
+					// $proceed: Name of the method originally called
+					// $$: all the arguments
+					m.replace("{ $_ = " + newMethodName + "($$); }");
+					
+					// Create new method "$debug"
+					CtMethod newDebuggedMethod = null;
+					
+					try {
+						newDebuggedMethod = CtNewMethod.copy(m.getMethod(), newMethodName, ctClass, null);
+					} catch (NotFoundException e) {
+						e.printStackTrace();
+					}
+					
+					newDebuggedMethod.setBody(
+							"try {" +
+							"	return ($r) "+ methodName + "($$);" +
+							"} catch (java.lang.Exception e){"+
+							"	Object returnObject;" +
+							"	try {" +
+							"		returnObject = ist.meic.pa.DebuggerCLI.initCommandLine(this, e); " +
+							"		return ($r) returnObject; " +
+							"	} catch (DCLIThrowable t){ " +
+							"		if(t.getValue() == \"RETRY\"){" + 
+							"			" + newMethodName + "($$);" + 
+							"		}" + 
+							"	}" +
+							"}");
+					
+					ctClass.addMethod(newDebuggedMethod);
+								
+					// Add to the map
+					debuggedMethods.put(methodName, "");
+
+					
+				}
+			});
+			
 		}
 	}	
 
